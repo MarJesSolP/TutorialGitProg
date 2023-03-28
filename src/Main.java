640e19a40ba16ac274626e4707780d6ed9f9e368
MarJesSolP <usr3597@salesianos-lacuesta.net>
2023-03-28 10:43:08 +0100

    feet(Main): added user number input to sout

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[]args){
-        System.out.println("Holaa Mundo");
-
-
+        final Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduce un numero ");
+        int number = teclado.nextInt();
+        System.out.println("Holaa Mundo " + number + "!");
+        teclado.close();
     }
 }
